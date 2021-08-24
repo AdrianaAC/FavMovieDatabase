c2ef63f8c6fa2fee826768f900343106023c1dae
AdrianaAlves <adrianaalves098@gmail.com>
2021-08-24 19:34:24 +0100

    Adding movies is now possible

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,58 @@
 const addMovieModal = document.getElementById("add-modal");
+const startAddMovieButton = document.querySelector("header button");
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton =  addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll("input");
+const movies = [];
 
-const startAddMobieButton = document.querySelector("header button");
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible")
+}
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const clearMovieInputs = () => {
+ for(const usrInput of userInputs) {
+   usrInput.value = "";
+ }
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+  
+  if(titleValue.trim() === "" || imageUrlValue.trim() === "" || ratingValue.trim() === "" || +ratingValue < 1 || +ratingValue > 5) {
+  alert("Please enter valid values, with a rating between 1 and 5");
+  return;
+  }
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+  
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModal();
+  
+  }
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+}
+
+
+
+startAddMovieButton.addEventListener("click", toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
+confirmAddMovieButton.addEventListener("click", addMovieHandler); 
